08d245aa790bb44fac0e25100f8b2967bc570c2e
Nmerey <merey.nurlan@gmail.com>
2023-08-07 14:39:50 +0600

    Change config

--- .platform/hooks/prebuild/03_install_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'Run yarn install'
-
-EB_APP_STAGING_DIR=$(sudo /opt/elasticbeanstalk/bin/get-config platformconfig -k AppStagingDir)
-cd $EB_APP_STAGING_DIR
-yarn install --production
