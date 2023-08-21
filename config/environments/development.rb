8445ee03c6e18152bc7c52ba6b960229596a2c7b
Nmerey <merey.nurlan@gmail.com>
2023-08-21 17:27:03 +0600

    Change production bot

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Default host for telegram webhook in development env
+  routes.default_url_options = {host: ENV["WEBHOOK_URL"] , protocol: 'https'}
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
