ea08433f1b615365c323e2a47b2dbbea02ab32e1
Merey Nurlan <32390632+Nmerey@users.noreply.github.com>
2023-11-23 13:02:12 +0000

    Rescue with standart error

--- app/controllers/concerns/validation_handler.rb
+++ app/controllers/concerns/validation_handler.rb
@@ -4,16 +4,14 @@ module ValidationHandler extend ActiveSupport::Concern
 		def check_date(date)
 			pattern =  /^\d{2}\.\d{2}$/
 			unless !!(date =~ pattern)
-				wrong_argument_error
-				raise ActiveJob::DeserializationError
+				raise wrong_argument_error rescue StandardError 
 			end
 		end
 
 		def check_change_rating_args(rating, player)
 			check_rating(rating)
 			unless player
-				wrong_argument_error
-				raise ActiveJob::DeserializationError
+				raise wrong_argument_error rescue StandardError
 			end
 		end
 
@@ -25,15 +23,13 @@ module ValidationHandler extend ActiveSupport::Concern
 		def check_rating(rating)
 			pattern = /^(10(\.0)?|\d(\.\d)?)$/ # Matches float number from 0 to 10.0
 			unless !!(rating =~ pattern)
-				wrong_argument_error
-				raise ActiveJob::DeserializationError
+				raise wrong_argument_error rescue StandardError
 			end
 		end
 
 		def check_division_args(teams_count, players_count, total_players)
 			unless (players_count % teams_count) == 0 && players_count <= total_players
-				wrong_argument_error
-				raise ActiveJob::DeserializationError
+				raise wrong_argument_error rescue StandardError
 			end
 		end
 
